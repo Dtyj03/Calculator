d9088fe022e2132faff7d8dce5e0eebc53425687
Dtyj <80689943+Dtyj03@users.noreply.github.com>
2023-06-15 16:42:51 +0800

    Delete OperationBtn.js

--- OperationBtn.js
+++
@@ -1,9 +0,0 @@
-import { CALCULATE } from "./App"
-
-export default function OperationBtn({ dispatch, operation }){
-    return(
-    <button onClick={() => dispatch({ type: CALCULATE.CHOOSE_OPERATION, payload :{ operation } })}>
-        {operation}
-        </button>
-    )
-}
