2434924c00a6f30bf0b06112a3c595184b9c6b01
Dtyj <80689943+Dtyj03@users.noreply.github.com>
2023-06-15 16:42:43 +0800

    Delete DigitBtn.js

--- DigitBtn.js
+++
@@ -1,9 +0,0 @@
-import { CALCULATE } from "./App"
-
-export default function DigitBtn({ dispatch, digit}){
-    return(
-    <button onClick={() => dispatch({ type: CALCULATE.ADD_DIGIT, payload :{ digit } })}>
-        {digit}
-        </button>
-    )
-}
